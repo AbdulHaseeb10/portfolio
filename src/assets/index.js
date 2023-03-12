2a1fb88957883249e0f0e284b8e68f04142e37eb
Abdul Haseeb <mohammedabdulhaseeb4@gmail.com>
2023-03-12 17:08:00 +0530

    Initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,71 @@
+import logo from "./logo.png";
+import codebot from './codebot.jpeg';
+import aicanvas from  "./aicanvas.png";
+import fakenews from "./fakenews.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import java from './tech/java.png';
+import python from './tech/python.png';
+import cpp from './tech/cpp.png';
+import menu from "./menu.svg";
+import close from "./close.svg";
+import stakesmen from "./company/stakesmen.png";
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  codebot,
+  aicanvas,
+  fakenews,
+  github,
+  java,
+  python,
+  cpp,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  stakesmen,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,191 @@
+import {
+    web,
+    javascript,
+    codebot,
+    aicanvas,
+    fakenews,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    carrent,
+    stakesmen,
+    jobit,
+    tripguide,
+    threejs,
+    java,
+    python,
+    cpp
+} from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    }
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name:"Java",
+      icon:java,
+    },
+    {
+      name:"C++",
+      icon: cpp,
+    },
+    {
+      name:"Python",
+      icon: python,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    
+  ];
+  
+  const experiences = [
+    {
+      title: "Web Developer",
+      company_name: "stakesmen.com",
+      icon: stakesmen,
+      iconBg: "#fff",
+      date: "September 2022 - February 2023",
+      points: [
+        "Creating responsive frontend components using react.",
+        "Configuring the state of the application using redux-toolkit.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Writing unit test-cases using react testing library.",
+      ],
+    }
+  ];
+  
+  
+  const projects = [
+    {
+      name: "Fake News Detection",
+      description:
+        "Fake news detection system that examines whether a given news article is real or fake. Preprocessed and cleaned the dataset using Natural language processing methods. Trained the dataset using LSTM deep learning model with 97% accuracy",
+      tags: [
+        {
+          name: "python",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "tensorflow",
+          color: "green-text-gradient",
+        },
+        {
+          name: "deep learning",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: fakenews,
+      source_code_link: "https://github.com/AbdulHaseeb10/Fake-News-Detection",
+    },
+    {
+      name: "CodeBot",
+      description:
+        "A chatgpt like application that understands natural language and responds with intelligent answers. Used the openai gpt-3’s text-davinci-003 model. Integrated the application with nodejs backend and html,css and javscript frontend.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "gpt-3",
+          color: "green-text-gradient",
+        },
+        {
+          name: "openai",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: codebot,
+      source_code_link: "https://github.com/AbdulHaseeb10/CodeBot",
+    },
+    {
+      name: "AI-Canvas",
+      description:"A MERN AI application that generates images to your queries and let you showcase the images to community. Used Reactjs for frontend and Nodejs for backend, MongoDB as a database and cloudinary for storage.",
+        tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "dalle",
+          color: "green-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: aicanvas,
+      source_code_link: "https://github.com/AbdulHaseeb10/AI-Canvas",
+    },
+  ];
+  
+  export { services, technologies, experiences, projects };
+  
